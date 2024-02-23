dce24a94622a31a875902be4690798d41a5dd02e
Precious Kusi <146889458+preciouskusi17@users.noreply.github.com>
2024-02-22 23:48:41 -0500

    Edit index.html, style.css, and java.js
    Added pattern to password input so that it will contain at least
    1 lower case, 1 upper case, and 1 digit

--- java.js
+++ java.js
@@ -6,8 +6,6 @@ let password2=document.querySelector('.password2');
 
 password1.addEventListener('keyup', ()=>{
     if (password1.value==password2.value &&password1.value!=='' && password2.value !==''){
-        password1.setAttribute('style','outline: none; border: solid green 3px;');
-        password2.setAttribute('style','outline: none; border: solid green 3px;');
         div.textContent='';
     }else if(password1.value == '' | password2.value == ''){
         div.textContent='Password is required'
@@ -18,8 +16,6 @@ password1.addEventListener('keyup', ()=>{
 });
 password2.addEventListener('keyup', ()=>{
     if (password1.value==password2.value &&password1.value!=='' && password2.value !==''){
-        password1.setAttribute('style','outline: none; border: solid green 3px;');
-        password2.setAttribute('style','outline: none; border: solid green 3px;');
         div.textContent='';
     }else if(password1.value == '' | password2.value == ''){
         div.textContent='Password is required'
